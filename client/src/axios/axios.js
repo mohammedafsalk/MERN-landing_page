ab9f5760757f6f105811f675dc0495e802c7b308
mohammedafsalk <techafs456@gmail.com>
2023-12-28 15:12:26 +0530

    baseurl changed to deployed

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = (tokenName) => {
   const instance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://landings-ftje.onrender.com/",
     timeout: 5000,
     headers: {
       "Content-Type": "application/json",
